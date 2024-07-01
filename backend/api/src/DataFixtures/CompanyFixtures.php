9d9240b0c6d60b8112f81f260bd8c41af0e2fa03
riad <riad.ahmedyahia@yahoo.fr>
2024-07-01 11:40:32 +0200

    ajustment fixture company

--- backend/api/src/DataFixtures/CompanyFixtures.php
+++ backend/api/src/DataFixtures/CompanyFixtures.php
@@ -21,8 +21,8 @@ class CompanyFixtures extends Fixture
     {
         $company1 = new Company();
         $company1->setName('Company One');
-        $company1->setFoundationDate(2000);
-        $company1->setCountries(3);
+        $company1->setFoundationDate('2024-30-06'); 
+        $company1->setCountry("Spain");
         $company1->setRaised('1M USD');
         $company1->setEmail('company1@example.com');
         $company1->setPassword($this->passwordHasher->hashPassword($company1, 'password1'));
@@ -35,8 +35,8 @@ class CompanyFixtures extends Fixture
 
         $company2 = new Company();
         $company2->setName('Company Two');
-        $company2->setFoundationDate(2010);
-        $company2->setCountries(5);
+        $company2->setFoundationDate('2024-20-06'); 
+        $company2->setCountry("France");
         $company2->setRaised('5M USD');
         $company2->setEmail('company2@example.com');
         $company2->setPassword($this->passwordHasher->hashPassword($company2, 'password2'));
